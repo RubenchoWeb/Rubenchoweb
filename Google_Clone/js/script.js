491310043443757d5c014e6247a2959946dc54b5
Rubenchoweb <rubenchoweb@outlook.com>
2021-03-10 12:57:46 -0500

    Usando otra API para obtener pais

--- Google_Clone/js/script.js
+++ Google_Clone/js/script.js
@@ -1,21 +1,16 @@
-var endpoint = 'http://ip-api.com/json/?fields=1163583';
+fetch('https://ipapi.co/json/')
+.then(function(response) {
+  response.json().then(jsonData => {
+    console.log(jsonData);
+    // Get country
+    var country = document.getElementById("country");
+    var api_country = jsonData.country_name;
+    console.log('Pais Api ' + api_country);
+    document.getElementById("country").innerText = api_country;
+  });
+})
+.catch(function(error) {
+  console.log(error)
+});
+    
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) 
-    {
-        var response = JSON.parse(this.responseText);
-        if(response.status !== 'success') 
-        {
-            console.log('query failed: ' + response.message);
-            return
-        }
-        // Get country
-        var country = document.getElementById("country");
-        var api_country = response.country;
-        console.log('Pais Api ' + api_country);
-        document.getElementById("country").innerText = api_country;
-    }
-};
-xhr.open('GET', endpoint, true);
-xhr.send();
