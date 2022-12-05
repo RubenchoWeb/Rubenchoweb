c8a73a8586669b67cee132c89bbd7b50712c53a0
Rubenchoweb <rubenchowe@outlook.com>
2022-12-04 22:17:50 -0500

    Read and Insert code

--- UCamp/CRUD/script.js
+++ UCamp/CRUD/script.js
@@ -7,16 +7,64 @@ function Submit() {
   var dataEntered = retrieveData();
   var readData = readingDataFromLocalStorage(dataEntered);
   if (dataEntered == false) {
-    msg.innerHTML = `<h3 style = "color: red;">Please enter data !</h3>`;
+    msg.innerHTML = `<h3 style = "color: red;">Ingresa los datos !</h3>`;
   } else {
     if (row == null) {
       insert(readData);
-      msg.innerHTML = `<h3 style = "color: yellow;">Data Inserted !</h3>`;
+      msg.innerHTML = `<h3 style = "color: yellow;">Datos ingresados !</h3>`;
     } else {
       update();
-      msg.innerHTML = `<h3 style = "color: orange;">Data Updated !</h3>`;
+      msg.innerHTML = `<h3 style = "color: orange;">Datos actualizados !</h3>`;
     }
   }
 
   document.getElementById("form").reset();
 }
+
+// READ
+// Retrieve data
+function retrieveData() {
+    var name1 = document.getElementById("name").value;
+    var job = document.getElementById("job").value;
+    var exp = document.getElementById("exp").value;
+  
+    var arr = [name1, job, exp];
+    if (arr.includes("")) {
+      return false;
+    } else {
+      return arr;
+    }
+  }
+  
+  //Data in Local Storage
+  function readingDataFromLocalStorage(dataEntered) {
+    // Storing data in local storage
+    var n = localStorage.setItem("Name", dataEntered[0]);
+    var j = localStorage.setItem("Job", dataEntered[1]);
+    var e = localStorage.setItem("Experience", dataEntered[2]);
+  
+    // Show data in table (Getting item from localStorage)
+    var n1 = localStorage.getItem("Name", n);
+    var j1 = localStorage.getItem("Job", j);
+    var e1 = localStorage.getItem("Experience", e);
+  
+    var arr = [n1, j1, e1];
+    return arr;
+  }
+  
+  // INSERT
+  function insert(readData) {
+    var table = document.getElementById("table");
+    var i = table.rows.length;
+    var row = table.insertRow(i);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    // row.insertCell(4).innerHTML = "JJJ"
+    cell1.innerHTML = readData[0];
+    cell2.innerHTML = readData[1];
+    cell3.innerHTML = readData[2];
+    cell4.innerHTML = `<button onclick="edit(this)"><a href="script.js:void(0)" style="text-decoration: none;">Edit</a></button> &nbsp
+  <button onclick="remove(this)"><a href="script.js:void(0)" style="text-decoration: none;">Delete</a></button>`;
+  }
